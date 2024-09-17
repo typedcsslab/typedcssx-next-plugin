64415c2047a96165c5c7be0a9f28ecb5e7d329b4
S.Watanabe <ocelot2828@gmail.com>
2024-09-17 16:11:00 +0900

    feat(config-css-module.ts): Return only local of CSSModule

---
+++ src/plugin/config-css-module.ts
@@ -0,0 +1,51 @@
+import { Configuration, RuleSetRule, RuleSetUseItem } from "webpack";
+
+type RuleWithUse = RuleSetRule & { use: RuleSetUseItem[] };
+type RuleWithOneOf = RuleSetRule & { oneOf: RuleSetRule[] };
+
+export function configCSSModule(config: Configuration): Configuration {
+  const rules = (
+    config.module?.rules
+      ?.filter(
+        (rule): rule is RuleWithOneOf =>
+          typeof rule === "object" &&
+          rule?.oneOf !== undefined &&
+          Array.isArray(rule.oneOf)
+      )
+      ?.flatMap((rule) => rule.oneOf || []) || []
+  ).filter(
+    (rule): rule is RuleWithUse =>
+      typeof rule === "object" && Array.isArray(rule.use)
+  );
+
+  rules.forEach((rule) => {
+    rule.use.forEach((moduleLoader) => {
+      if (
+        typeof moduleLoader === "object" &&
+        moduleLoader!.loader?.includes("css-loader")
+      ) {
+        const cssLoader = moduleLoader;
+
+        if (
+          typeof cssLoader?.options === "object" &&
+          cssLoader.options !== null
+        )
+          cssLoader.options = {
+            ...cssLoader.options,
+            modules: {
+              ...cssLoader.options.modules,
+              getLocalIdent: (
+                _context: any,
+                _localIdentName: string,
+                localName: string
+              ) => {
+                return localName;
+              },
+            },
+          };
+      }
+    });
+  });
+
+  return config;
+}
